3fd7c88c9d389953851e266e319327c25d798f5c
pinwormmy <ozkhan@naver.com>
2023-02-13 15:58:43 +0900

    Spring HATEOAS 적용중...
    EntityModel class로 적용 하나 해봤는데,
    하드 코딩 형태라 실제론 다른 방법으로 적용된다
    
    여러가지방법 한번씩 다 써보고, 제일 실용적인 방식 익히기

--- src/main/java/com/mall/petshop/product/ReviewController.java
+++ src/main/java/com/mall/petshop/product/ReviewController.java
@@ -3,20 +3,23 @@ package com.mall.petshop.product;
 import com.mall.petshop.util.PageDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
+@RequestMapping(produces = MediaTypes.HAL_JSON_VALUE)
 public class ReviewController {
 
     @Autowired
     ProductService productService; // 추후 ReviewService로 따로 정리하기
 
-    @PostMapping(value="/reviews", produces = MediaTypes.HAL_JSON_VALUE)
+    @PostMapping(value="/reviews")
     public void addReview(@RequestBody ReviewDTO reviewDTO) throws Exception {
         log.debug("리뷰 인수 확인 : {}", reviewDTO.getContent());
         productService.addReview(reviewDTO);
@@ -28,9 +31,13 @@ public class ReviewController {
     }
 
     @GetMapping(value= "/reviews/{productNum}") // PathVariable에선 오히려 카멜케이스 권장. 어차피 JSON에서 DTO기준으로 떠서 카멜로 뜨게됨
-    public List<ReviewDTO> showReviewList(@PathVariable int productNum) throws Exception {
+    public List<EntityModel<ReviewDTO>> showReviewList(@PathVariable int productNum) throws Exception {
         log.debug("리뷰 컨트롤러 작동 확인 : {}", productNum); // 비동기로 하면 인텔리제이 로그에 안뜸
-        return productService.showReviewList(productNum);
+        return productService.showReviewList(productNum).stream().map(reviewDTO -> {
+            EntityModel<ReviewDTO> entityModel = EntityModel.of(reviewDTO);
+            entityModel.add(Link.of("/reviews", "list"));
+            return entityModel;
+        }).collect(Collectors.toList());
     }
 
     @DeleteMapping(value="/reviews/{reivewNum}")
