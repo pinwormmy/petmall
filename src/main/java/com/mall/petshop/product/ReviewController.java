61c6a23df0f647659530ab86fdca7a6d7675afb5
pinwormmy <ozkhan@naver.com>
2023-02-13 16:20:58 +0900

    Spring HATEOAS 적용중...
    WebMvcLinkBuilder를 써보려는데, 불러올 데이터가 List형태라 변형을 해야함
    
    여러가지방법 한번씩 다 써보고, 제일 실용적인 방식 익히기

--- src/main/java/com/mall/petshop/product/ReviewController.java
+++ src/main/java/com/mall/petshop/product/ReviewController.java
@@ -3,6 +3,7 @@ package com.mall.petshop.product;
 import com.mall.petshop.util.PageDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.MediaTypes;
@@ -10,6 +11,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Slf4j
 @RestController
@@ -30,14 +34,11 @@ public class ReviewController {
         return productService.pageSetting(page);
     }
 
+    // CollectionModel의 경우, 낱개로 가져오는 api를 뭉쳐서 목록 api 구성하는 방식인데, 리뷰는 낱개로 불러오지않는다. 나중에 다시 테스트
     @GetMapping(value= "/reviews/{productNum}") // PathVariable에선 오히려 카멜케이스 권장. 어차피 JSON에서 DTO기준으로 떠서 카멜로 뜨게됨
     public List<EntityModel<ReviewDTO>> showReviewList(@PathVariable int productNum) throws Exception {
         log.debug("리뷰 컨트롤러 작동 확인 : {}", productNum); // 비동기로 하면 인텔리제이 로그에 안뜸
-        return productService.showReviewList(productNum).stream().map(reviewDTO -> {
-            EntityModel<ReviewDTO> entityModel = EntityModel.of(reviewDTO);
-            entityModel.add(Link.of("/reviews", "list"));
-            return entityModel;
-        }).collect(Collectors.toList());
+        // WebMvcLinkBuilder를 List로 묶어서 구현해보기
     }
 
     @DeleteMapping(value="/reviews/{reivewNum}")
