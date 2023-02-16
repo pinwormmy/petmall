0fbb6eb7be60afc08af49932594ac0f03f4e36b1
pinwormmy <ozkhan@naver.com>
2023-02-16 14:44:17 +0900

    Spring HATEOAS 적용중. 댓글추가 컨트롤러에 적용함
    postmapping에 추가
    RepresentationModelAssembler 굳이 쓸 필요없이,
    WebMvcLinkBuilder로 경로 편하게 추가했고,
    ResponseEntity로 http status추가에, 리턴 후 경로, 표시할 json
    한번에 넣을 수 있음. 이게 제일 효율적인 형태로 보임.
    EntityModel에 경로 표현을 위해서 리뷰컨트롤러 전체 경로 넣고,
    각 컨트롤러에 중복되는 reviviews 단어 다 뺌
    리팩토링 차원에서도 이게 좋은듯
    
    #로그 레벨 설정 경로 잘못 지정되어있어서 debug문 출력 안되던것 수정함

--- src/main/java/com/mall/petshop/product/ReviewController.java
+++ src/main/java/com/mall/petshop/product/ReviewController.java
@@ -2,15 +2,15 @@ package com.mall.petshop.product;
 
 import com.mall.petshop.util.EntityToModelConverter;
 import com.mall.petshop.util.PageDTO;
+import com.sun.jndi.toolkit.url.Uri;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.MediaTypes;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,7 +18,7 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 @Slf4j
 @RestController
-@RequestMapping(produces = MediaTypes.HAL_JSON_VALUE)
+@RequestMapping(value="/reviews", produces = MediaTypes.HAL_JSON_VALUE)
 public class ReviewController {
 
     @Autowired
@@ -27,24 +27,27 @@ public class ReviewController {
     EntityToModelConverter entityToModelConverter;
 
 
-    @PostMapping(value="/reviews") // 의도한데로 결과값이 안나오고 있다. 대충 복붙하려고 말고 필요한거 하나씩 가져오면서 공부
+    @PostMapping
     public ResponseEntity addReview(@RequestBody ReviewDTO reviewDTO) {
-        log.debug("리뷰 인수 확인 : {}", reviewDTO.getContent());
+        log.info("댓글 추가");
         productService.addReview(reviewDTO);
-        HttpHeaders headers = new HttpHeaders();
-        headers.setLocation(linkTo(ReviewController.class).slash(reviewDTO).toUri());
-        return new ResponseEntity(headers, HttpStatus.CREATED);
+        URI uri = linkTo(ReviewController.class).slash(showReviewList(reviewDTO.getProductNum())).toUri();
+        EntityModel<ReviewDTO> entityModel = EntityModel.of(
+                reviewDTO,
+                linkTo(ReviewController.class).withRel("reviewList")
+        );
+        return ResponseEntity.created(uri).body(entityModel);
     }
 
+//Generic types shouldn’t be used raw (without type parameters) in variable declarations or return values. Doing so bypasses generic type checking, and defers the catch of unsafe code to runtime.
 
-
-    @PostMapping(value = "/reviews/page") // 페이징 처리 방식을 아예 바꿔야함
+    @PostMapping(value = "/page") // 페이징 처리 방식을 아예 바꿔야함
     public PageDTO reviewPageSetting(@RequestBody PageDTO page) throws Exception {
         return productService.pageSetting(page);
     }
 
     // CollectionModel의 경우, 낱개로 가져오는 api를 뭉쳐서 목록 api 구성하는 방식인데, 리뷰는 낱개로 불러오지않는다. 나중에 다시 테스트
-    @GetMapping(value= "/reviews/{productNum}") // PathVariable에선 오히려 카멜케이스 권장. 어차피 JSON에서 DTO기준으로 떠서 카멜로 뜨게됨
+    @GetMapping(value= "/{productNum}") // PathVariable에선 오히려 카멜케이스 권장. 어차피 JSON에서 DTO기준으로 떠서 카멜로 뜨게됨
     public List<EntityModel<ReviewDTO>> showReviewList(@PathVariable int productNum) {
         log.debug("리뷰 컨트롤러 작동 확인 : {}", productNum); // 비동기로 하면 인텔리제이 로그에 안뜸
         return productService.showReviewList(productNum)
@@ -53,12 +56,12 @@ public class ReviewController {
                 .collect(Collectors.toList());
     }
 
-    @DeleteMapping(value="/reviews/{reivewNum}")
+    @DeleteMapping(value="/{reivewNum}")
     public void deleteReview(@PathVariable int reivewNum) {
         productService.deleteReview(reivewNum);
     }
 
-    @PutMapping(value = "/reviews/count/{productNum}")
+    @PutMapping(value = "/count/{productNum}")
     public void updateReviewCount(@PathVariable int productNum) {
         log.debug("댓글수업데이트 컨트롤러 작동 확인");
         productService.updateReviewCount(productNum);
